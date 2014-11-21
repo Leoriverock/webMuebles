e56228618bc9dbd5ffb700eb2336b99be84d339d
Leo Rivero <mig41189@gmail.com>
2014-11-20 22:26:51 -0200

    Pagina web para Sermanfor - sitio de informacion de productos

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Sermanfor</title>
+                <meta charset="UTF-8">
+                
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<script src="js/holder.js"></script>
+		<script type="text/javascript" src ="js/bootstrap.js"></script>
+		
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+			<link rel="stylesheet" href="css/style-noscript.css" />
+		</noscript>
+    </head>
+    <body>
+        <!-- Wrapper-->
+			<div id="wrapper">
+				<div id="banner">
+					
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner">
+					<div class="item active">
+					  <img class="img-thumbnail" src="images/img1.jpg" alt="">
+					  
+					</div>
+					<div class="item">
+					  <img class="img-thumbnail" src="images/img2.jpg" alt="">
+					  
+					</div>
+					<div class="item">
+					  <img class="img-thumbnail" src="images/img3.jpg" alt="">
+					  
+					</div>
+					
+				  </div>
+			</div>
+				
+				<!-- Nav -->
+					<nav id="nav">
+						<a href="#me" class="icon fa-home active"><span>Inicio</span></a>
+						<a href="#gps" class="icon fa-crosshairs"><span>Ubicacion</span></a>
+						<a href="#products" class="icon fa-credit-card"><span>Productos</span></a>
+						<a href="#contact" class="icon fa-envelope"><span>Contacto</span></a>
+						
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						
+						<!-- Me -->
+							<article id="me" class="panel">
+								<header>
+									<img src="images/me.png"  height="150" width="315" />
+								</header>
+								
+								<a href="#products" class="jumplink pic">
+									<span class="arrow icon fa-chevron-right"><span>Productos</span></span>
+									<img src="holder.js/300x400/vine" />
+								</a>
+							</article>
+							
+						<!-- Gps -->
+						<article id="gps" class="panel">
+							<header>
+									<h2>Ubicacion</h2>
+							</header>
+							<div class="row">
+                                                            <div class="6u">
+                                                                <img src="holder.js/550x550/industrial" alt="">
+                                                                <p><span>He aqui la ubicacion de nuestras instalaciones</span></p>
+                                                            </div>
+                                                        </div>
+						</article
+						
+
+						<!-- Products --> 
+							<article id="products" class="panel">
+								<header>
+									<h2>Productos</h2>
+								</header>
+								<p>
+									He aqui una gran variedad de productos realizados en madera de buena calidad
+									disponibles a la venta.
+								</p>
+								<section>
+									<div class="row">
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/vine"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/sky"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/lava"></a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/vine"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/sky"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/lava"></a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/vine"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/sky"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/lava"></a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/vine"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/sky"></a>
+										</div>
+										<div class="4u">
+											<a href="#" class="image fit"><img src="holder.js/250x250/lava"></a>
+										</div>
+									</div>
+								</section>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact" class="panel">
+								<header>
+									<h2>Contacto</h2>
+								</header>
+								<form action="#" method="post">
+									<div>
+										<div class="row">
+											<div class="6u">
+												<input type="text" name="name" placeholder="Nombre" />
+											</div>
+											<div class="6u">
+												<input type="text" name="email" placeholder="Email" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="text" name="subject" placeholder="Asunto" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<textarea name="message" placeholder="Mensaje" rows="8"></textarea>
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="submit" value="Enviar Mensaje" />
+											</div>
+										</div>
+									</div>
+								</form>
+							</article>
+
+					</div>
+		
+				<!-- Footer -->
+					<div id="footer">
+						<ul class="copyright">
+							<p>&copy; 2014 La Pandilla Inc.</p>
+						</ul>
+					</div>
+		
+			</div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
